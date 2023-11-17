05b0bcea78f655439edaf4d2cb965110aed0ecf6
CarlosVF22 <cevasquezf22@gmail.com>
2023-11-17 09:43:11 -0500

    feat: :sparkles: first commit

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import LightTheme from "../layouts/Light";
+import Home from "./home/home4-light";
+
+export default function Index() {
+    return (
+        <LightTheme>
+            <Home />
+        </LightTheme>
+    );
+}
