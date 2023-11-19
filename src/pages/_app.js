57d576b3116276dc66d745d3968b0746c9192fb0
CarlosVF22 <cevasquezf22@gmail.com>
2023-11-18 22:32:35 -0500

    style: :construction: work in styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,41 +7,43 @@ import LoadingScreen from "../components/Loading-Screen";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Avo</title>
-        <link rel="icon" href="/img/favicon.ico" />
-      </Head>
-      <Cursor />
-      <LoadingScreen />
-      <ScrollToTop />
-      <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Creadtive Agency</title>
+                <link rel="icon" href="/img/favicon.ico" />
+            </Head>
+            <Cursor />
+            <LoadingScreen />
+            <ScrollToTop />
+            <Component {...pageProps} />
 
-      <Script
-        strategy="beforeInteractive"
-        id="wow"
-        src="/js/wow.min.js"
-      >
-      </Script>
-      <Script
-        strategy="beforeInteractive"
-        id="splitting"
-        src="/js/splitting.min.js"
-      ></Script>
-      <Script id="simpleParallax" src="/js/simpleParallax.min.js"></Script>
-      <Script
-        strategy="beforeInteractive"
-        id="isotope"
-        src="/js/isotope.pkgd.min.js"
-      ></Script>
-      <Script
-        strategy="lazyOnload" 
-        id="initWow" 
-        src="/js/initWow.js"
-      ></Script>
-    </>
-  );
+            <Script
+                strategy="beforeInteractive"
+                id="wow"
+                src="/js/wow.min.js"
+            ></Script>
+            <Script
+                strategy="beforeInteractive"
+                id="splitting"
+                src="/js/splitting.min.js"
+            ></Script>
+            <Script
+                id="simpleParallax"
+                src="/js/simpleParallax.min.js"
+            ></Script>
+            <Script
+                strategy="beforeInteractive"
+                id="isotope"
+                src="/js/isotope.pkgd.min.js"
+            ></Script>
+            <Script
+                strategy="lazyOnload"
+                id="initWow"
+                src="/js/initWow.js"
+            ></Script>
+        </>
+    );
 }
 
 export default MyApp;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,58 +1,52 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return {
-      ...initialProps,
-    };
-  }
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return {
+            ...initialProps,
+        };
+    }
 
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="keywords"
-            content="HTML5 Template Avo onepage themeforest"
-          />
-          <meta
-            name="description"
-            content="Avo - Onepage Multi-Purpose HTML5 Template"
-          />
-          <meta name="author" content="" />
-          <link rel="shortcut icon" href="/img/favicon.ico" />
-          {/* Google Fonts */}
-          <link
-            href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Barlow:wght@100;200;300;400;500;600;700;800;900&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css?family=Playfair+Display:400,500,600,700,800,900&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@200;300;400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                    <meta name="keywords" content="Creadtive Agency" />
+                    <meta name="description" content="Creadtive agency" />
+                    <meta name="author" content="" />
+                    <link rel="shortcut icon" href="/img/favicon.ico" />
+                    {/* Google Fonts */}
+                    <link
+                        href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
+                        rel="stylesheet"
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Barlow:wght@100;200;300;400;500;600;700;800;900&display=swap"
+                        rel="stylesheet"
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css?family=Playfair+Display:400,500,600,700,800,900&display=swap"
+                        rel="stylesheet"
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap"
+                        rel="stylesheet"
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@200;300;400;500;600;700&display=swap"
+                        rel="stylesheet"
+                    />
+                </Head>
 
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
 
 export default MyDocument;
