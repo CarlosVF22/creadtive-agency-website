62b3424486532543b9ce447ad6306c5b1d641248
CarlosVF22 <cevasquezf22@gmail.com>
2023-11-27 20:49:40 -0500

    feat: Add @vercel/analytics for tracking website usage
    This commit adds the @vercel/analytics package to the project
    dependencies and includes the Analytics component on the _app.js page.
    This package enables tracking and analysis of website usage. By
    incorporating this functionality, we can gain valuable insights into
    user behavior and make informed decisions about improving the website.
    The addition of this package reinforces our commitment to data-driven
    decision-making.
    
    References: #123

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,45 +5,50 @@ import Cursor from "../components/Cursor";
 import ScrollToTop from "../components/Scroll-to-top";
 import LoadingScreen from "../components/Loading-Screen";
 import { LanguageProvider } from "../common/languageContext";
+import { Analytics } from "@vercel/analytics/react";
+
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
     return (
-        <LanguageProvider>
-            <Head>
-                <title>Creadtive Agency</title>
-                <link rel="icon" href="/img/favicon.ico" />
-            </Head>
-            <Cursor />
-            <LoadingScreen />
-            <ScrollToTop />
-            <Component {...pageProps} />
+        <>
+            <LanguageProvider>
+                <Head>
+                    <title>Creadtive Agency</title>
+                    <link rel="icon" href="/img/favicon.ico" />
+                </Head>
+                <Cursor />
+                <LoadingScreen />
+                <ScrollToTop />
+                <Component {...pageProps} />
 
-            <Script
-                strategy="beforeInteractive"
-                id="wow"
-                src="/js/wow.min.js"
-            ></Script>
-            <Script
-                strategy="beforeInteractive"
-                id="splitting"
-                src="/js/splitting.min.js"
-            ></Script>
-            <Script
-                id="simpleParallax"
-                src="/js/simpleParallax.min.js"
-            ></Script>
-            <Script
-                strategy="beforeInteractive"
-                id="isotope"
-                src="/js/isotope.pkgd.min.js"
-            ></Script>
-            <Script
-                strategy="lazyOnload"
-                id="initWow"
-                src="/js/initWow.js"
-            ></Script>
-        </LanguageProvider>
+                <Script
+                    strategy="beforeInteractive"
+                    id="wow"
+                    src="/js/wow.min.js"
+                ></Script>
+                <Script
+                    strategy="beforeInteractive"
+                    id="splitting"
+                    src="/js/splitting.min.js"
+                ></Script>
+                <Script
+                    id="simpleParallax"
+                    src="/js/simpleParallax.min.js"
+                ></Script>
+                <Script
+                    strategy="beforeInteractive"
+                    id="isotope"
+                    src="/js/isotope.pkgd.min.js"
+                ></Script>
+                <Script
+                    strategy="lazyOnload"
+                    id="initWow"
+                    src="/js/initWow.js"
+                ></Script>
+            </LanguageProvider>
+            <Analytics />
+        </>
     );
 }
 
