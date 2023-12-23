45ffa585735e169dcd8711613b09e6f9546c2f4d
CarlosVF22 <cevasquezf22@gmail.com>
2023-12-23 10:32:50 -0500

    fix: :bug: delete column quote_file_path in db

---
+++ prisma/migrations/20231223152915_delete_column_quote_file_path_in_table_quote/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `quote_file_path` on the `Quote` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Quote" DROP COLUMN "quote_file_path";
